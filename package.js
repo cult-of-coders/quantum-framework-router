437283781c8913bd7e675a905f5b8ee48329ebe5
Theodor Diaconu <diaconu.theodor@gmail.com>
2016-05-26 10:47:39 +0300

    updated package versions

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
     // Brief, one-line summary of the package.
     summary: 'Based on Flow Router, this plugin allows you to easily create routes within Quantum Framework',
     // URL to the Git repository containing the source code for this package.
-    git: '',
+    git: 'https://github.com/cult-of-coders/quantum-framework-router.git',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
@@ -16,7 +16,7 @@ Package.onUse(function (api) {
     api.use([
         'ecmascript',
         'templating',
-        'cultofcoders:quantum-framework',
+        'cultofcoders:quantum-framework@1.1.8_5',
         'kadira:flow-router@2.12.1',
         'kadira:blaze-layout@2.3.0'
     ]);
